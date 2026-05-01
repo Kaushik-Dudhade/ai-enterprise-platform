ce1bdfe9b0f53d5b2430f5868670e76bfbd8d310
Kaushik Dudhade <kaushik.dudhade@gmail.com>
2026-05-02 00:04:58 +0530

    Setup Postgresql database, Session Management, User Models and Schemas, User creation API with Database persistence

--- backend/app/api/v1/auth.py
+++ backend/app/api/v1/auth.py
@@ -1,7 +1,26 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from backend.app.db.session import SessionLocal
+from backend.app.models.user import User
+from backend.app.schemas.user import UserCreate
 
 auth_router = APIRouter(prefix="/auth", tags=["Authentication"])
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 @auth_router.get("/login")
 def login_check():
     return {"message": "Login route initialized"}
+
+@auth_router.post("/create-user")
+def create_user(user: UserCreate, db: Session = Depends(get_db)):
+    new_user = User(name=user.name, email=user.email)
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,11 +1,15 @@
 from fastapi import FastAPI
 from backend.app.api.routes import router
 from backend.app.core.config import settings
+from backend.app.db.session import engine, Base
+from backend.app.models import user
 
 app = FastAPI(
     title=settings.APP_NAME,
     version=settings.APP_VERSION
 )
+
+Base.metadata.create_all(bind=engine)
 
 app.include_router(router)
 
